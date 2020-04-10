797ca63332c288878df18cf62d6d451f4e6adbc9
neethu-ms <msneethu2989@gmail.com>
2020-04-10 13:42:30 +0000

    Recreate backend with different view

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,5 @@
-let express = require('express');
-let router = express.Router();
+var express = require('express');
+var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
