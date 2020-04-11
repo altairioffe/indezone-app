725c28b74cdf5466409b8f211b5172d40c91bdfb
neethu-ms <msneethu2989@gmail.com>
2020-04-11 20:36:05 +0000

    Add route from db

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,19 +1,11 @@
 let express = require('express');
 let router = express.Router();
+let db = require('../db/models/index');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  // Comment out this line:
-  //res.send('respond with a resource');
-
-  // And insert something like this instead:
-  res.json([{
-  	id: 1,
-  	username: "samsepi0l"
-  }, {
-  	id: 2,
-  	username: "D0loresH4ze"
-  }]);
+  db.user.create({id:5, email:"abc5@gmail.com",handle:'@user5'});
+ 
 });
 
 module.exports = router;
