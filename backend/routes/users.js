a3efa9f20f31f4da4c778c6e494276ba8359c724
neethu-ms <msneethu2989@gmail.com>
2020-04-14 17:02:18 +0000

    Add routes for users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,10 +2,40 @@ let express = require('express');
 let router = express.Router();
 let db = require('../db/models/index');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  db.user.create({id:5, email:"abc5@gmail.com",handle:'@user5'});
- 
+//Get users
+router.get("/", (req, res) => {
+  db.user.findAll().then(users => {
+    res.json(users);
+  })
+    .catch(err => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
+//Get specific user
+router.get("/:id", (req, res) => {
+  db.user.findOne({
+    where:{id:req.params.id}}).then(users => {
+    res.json(users);
+  })
+    .catch(err => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
+
+//Create  user
+router.post("/", (req, res) => {
+  db.user.create({handle:req.body.handle,email:req.body.email,password:req.body.password,points:req.body.points,journalNo:req.body.journalNo,
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString()
+  })
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+      res.status(500).json({ error: err.message });
+    });
 });
 
 module.exports = router;
