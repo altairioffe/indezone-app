42729e40c4e476ff2abaeb7263aed2b00ee0b5d6
Altair <altair.ioffe@gmail.com>
2020-04-15 05:31:21 +0000

    integrating Bio component into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
 import Bio from './components/Bio';
+import './App.css';
 
 class App extends Component {
   state = {users: []}
@@ -15,7 +15,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Bio />
+        <h1>Users</h1>
+       
+        <div>
+          <Bio />
+        </div>
+
         {this.state.users.map(user =>
           <div key={user.id}>{user.username}</div>
         )}
@@ -25,3 +30,35 @@ class App extends Component {
 }
 
 export default App;
+
+
+// import Bio from './components/Bio';
+// import QuestionList from "./components/QuestionList";
+// import Question from "./components/QuestionAnswer/Question";
+// import Index from "./components/QuestionAnswer/Index";
+// import "./App.css";
+
+
+// const questionsArr = [
+//   "Someone who needs me on my A-game today is...",
+//   "A situation that might stress me out or trip me up today could be...",
+//   "If I was a high performance coach looking at my life from a high level, I would tell myself to remember that ... ... and the way that my best self would deal with that is... ",
+//   "The big progects I have to keep in mind that I want to take on, even if I can't act toward them today, are...",
+//   "The big projects I have to keep in mind that I want to take on, even if I can't act toward them today, are...",
+//   "I would know that today was a great success if at the end of the day I did, or felt this..."
+// ];
+
+// const bio = "Everybody has the power to remodel their behaviour, habits, and attitudes, but not everybody knows how. Our app will make it simple and rewarding for anybody to get the benefits of reflective journaling. Our app will bring people together through personal goals, challenges, and insights, so that we can realize our potential together."
+
+// const questionsList = 
+//   questionsArr.map( (question, i) => {
+//     return(
+//       <Question
+//         key={i} 
+//         question={question}
+//         type={''}
+//         onEdit={()=>{}}
+//         onDelete={()=>{}}
+//       />
+//     )
+//   })
--- client/src/components/Bio.js
+++ client/src/components/Bio.js
@@ -14,9 +14,9 @@ import React from "react";
 export default function Bio(props){
   return (
     <main className="prompt__card prompt__card--create" id="bio">
-      
+      <div id="biopic-container">
       <img src={"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"} id="biopic" />
-      
+      </div>
         <section className="prompt__actions card_right">
             <h3 className="text--regular">{props.bio}</h3>
             <hr className="seperator"/>
