795b511a24a41c05107ded04103fb43cc575acb7
neethu-ms <msneethu2989@gmail.com>
2020-04-15 01:41:04 +0000

    Refactor code

--- backend/db/models/user_goal.js
+++ backend/db/models/user_goal.js
@@ -4,8 +4,8 @@ module.exports = (sequelize, DataTypes) => {
     user_id: DataTypes.INTEGER,
     goal_id: DataTypes.INTEGER,
     answer: DataTypes.STRING,
-    created_at: DataTypes.DATE,
-    updated_at: DataTypes.DATE
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE
   }, {});
   UserGoal.associate = function(models) {
     // associations can be defined here
