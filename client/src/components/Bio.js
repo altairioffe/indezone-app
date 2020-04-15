913af8fbfb43c9b6bcb7d8bde5670b58aba8b3c7
Altair <altair.ioffe@gmail.com>
2020-04-15 07:03:44 +0000

    add all basic elements for component and cleared scss until style is confirmed

--- client/src/components/Bio.js
+++ client/src/components/Bio.js
@@ -4,8 +4,7 @@ should accept the following props:
   bio:String
   username:String
   Level:Integer
-  onConfirm:Function to be called when the user clicks the Confirm button
-  onCancel:Function to be called when the user clicks the Cancel button
+
 */
 
 import React from "react";
@@ -13,16 +12,18 @@ import React from "react";
 
 export default function Bio(props){
   return (
-    <main className="prompt__card prompt__card--create" id="bio">
+    <main className="" id="">
       <div id="biopic-container">
-      <img src={"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"} id="biopic" />
+      <img src={"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"} id="" />
       </div>
-        <section className="prompt__actions card_right">
-            <h3 className="text--regular">{props.bio}</h3>
-            <hr className="seperator"/>
-            <h4 className="text--regular">{props.username}</h4>
-            <h4 className="text--regular">{props.level}</h4>
-        </section>
+      <hr className=""/>
+      <section className="">
+          <h4 className="">{props.username}</h4>
+          <h4 className="">{props.level}</h4>
+      </section>
+      <section>
+        <p>{props.bio}</p>
+      </section>
     </main>
   );
 }
