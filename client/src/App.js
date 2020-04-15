8dc0727681f19414d81ca58eac1d5e24f94f66b3
Altair <altair.ioffe@gmail.com>
2020-04-15 04:53:12 +0000

    import Bio into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Bio from './components/Bio';
 
 class App extends Component {
   state = {users: []}
@@ -14,7 +15,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Users</h1>
+        <Bio />
         {this.state.users.map(user =>
           <div key={user.id}>{user.username}</div>
         )}
