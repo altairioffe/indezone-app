5a8345b68f85bf5745846069ab62dd2531ed1f3f
Altair <altair.ioffe@gmail.com>
2020-04-15 04:17:40 +0000

    added basic structure for bio component

--- client/src/components/Bio.js
+++ client/src/components/Bio.js
@@ -1,1 +1,28 @@
- 
+ /* 
+should accept the following props:
+
+  bio:String
+  username:String
+  Level:Integer
+  onConfirm:Function to be called when the user clicks the Confirm button
+  onCancel:Function to be called when the user clicks the Cancel button
+*/
+
+import React from "react";
+//import "./Bio.scss"
+
+export default function Bio(props){
+  return (
+    <main className="prompt__card prompt__card--create" id="bio">
+      
+      <img src={"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"} id="biopic" />
+      
+        <section className="prompt__actions card_right">
+            <h3 className="text--regular">{props.bio}</h3>
+            <hr className="seperator"/>
+            <h4 className="text--regular">{props.username}</h4>
+            <h4 className="text--regular">{props.level}</h4>
+        </section>
+    </main>
+  );
+}
