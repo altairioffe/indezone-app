a99428f1057ae555e77115ad8a7b4d10fa7adf8b
Altair <altair.ioffe@gmail.com>
2020-04-15 20:30:45 +0000

    cleared classnames in bio.js

--- client/src/components/Bio.js
+++ client/src/components/Bio.js
@@ -10,12 +10,12 @@ import "./Bio.scss"
 
 export default function Bio(props){
   return (
-    <main className="" id="bio">
-      <div id="biopic-container">
-        <img id="biopic" src={"https://image.freepik.com/free-photo/woman-smiling-face-with-curly-hair_1139-582.jpg"} alt="profile pic"/>
+    <main className="" id="">
+      <div id="">
+        <img id="" src={"https://image.freepik.com/free-photo/woman-smiling-face-with-curly-hair_1139-582.jpg"} alt="profile pic"/>
       </div>
-      <hr className=""/>
-      <section className="">
+      <hr className="seperator"/>
+      <section className="prompt__actions card_right">
           <h4 className="">{props.username}</h4>
           <h4 className="">Level: {props.level}</h4>
           <button 
