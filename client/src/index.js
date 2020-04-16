db3e2479ecc87103f0c639603072ca59eda48a2d
neethu-ms <msneethu2989@gmail.com>
2020-04-16 21:50:09 +0000

    Modified App component name

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import Application from './components/Application';
+import './index.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <App />,
+  <Application />,
   document.getElementById('root')
 );
