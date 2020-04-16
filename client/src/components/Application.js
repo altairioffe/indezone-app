c3654ddbaca61f08f19133c71b6972a968e8db50
neethu-ms <msneethu2989@gmail.com>
2020-04-16 21:44:41 +0000

    Add Wall in Application component

---
+++ client/src/components/Application.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import useApplicationData from '../hooks/useApplicationData';
+import {getPreviousUserGoals} from '../helpers/selectors';
+
+
+export default function Application() {
+    const {
+    state
+  } = useApplicationData();
+
+  const currentUser = "6"; //temporary change 
+  
+  const previousUserGoals = getPreviousUserGoals(state.userGoals,state.goals,new Date(),currentUser);
+  
+  return (
+  
+   <Wall key={currentUser} userGoals={previousUserGoals} userId = {currentUser}/>
+  );
+
+}
