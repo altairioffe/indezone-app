4c46937d0e9241d537cadd50169507b8ce0aa05b
neethu-ms <msneethu2989@gmail.com>
2020-04-17 19:12:28 +0000

    Add wall style related constants

---
+++ client/src/components/WallStyles.js
@@ -0,0 +1,8 @@
+export const  styles = {
+  contentStyle:{backgroundColor:"#b3d9ff"},
+  bubbleStyle:{border:"2px solid #002266"},
+  lineColor:"#00008B",
+  lineStyle:{ background: '#004d99', width: 5 }
+};
+
+ 
