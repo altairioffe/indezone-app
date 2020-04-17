0b8784399b8354e62ac60ca34f6a7578c21fcc1c
neethu-ms <msneethu2989@gmail.com>
2020-04-17 12:13:16 +0000

    Move previous goal processing code to goal helper

---
+++ client/src/helpers/goalHelper.js
@@ -0,0 +1,18 @@
+// Finds previous user Goals ( All goals other than that of current day )
+import moment from 'moment';
+export function getPreviousUserGoals(userGoals, goals, currentDate, userId) {
+  const previousGoals = userGoals.filter(userGoal => {
+    return (moment(userGoal.createdAt).format('MM-DD-YYYY') < moment(currentDate).format('MM-DD-YYYY')) && (userGoal.user_id == userId);
+  }
+  );
+  let expandedPreviousGoals = previousGoals.map((previousGoal) => {
+    let expandedGoal = {};
+    expandedGoal.createdAt = previousGoal.createdAt;
+    expandedGoal.question = goals[previousGoal.goal_id].question;
+    expandedGoal.answer = previousGoal.answer;
+    return expandedGoal;
+
+  });
+  return expandedPreviousGoals;
+
+};
