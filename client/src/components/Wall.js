a872c984b2730bbe62b1d789457c046da342d069
neethu-ms <msneethu2989@gmail.com>
2020-04-17 15:50:29 +0000

    Add timeline

--- client/src/components/Wall.js
+++ client/src/components/Wall.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import WallItem from './WallItem';
+import {Timeline, TimelineEvent} from 'react-event-timeline'
 export default function Wall(props) {
   const userGoals = props.userGoals;
   const userGoalsView = userGoals.map(userGoal => {
     return (
+      <Timeline key={userGoal.user_id}>
       <WallItem key={userGoal.id} createdAt={userGoal.createdAt} question={userGoal.question} answer={userGoal.answer} />
+      </Timeline>
     );
   }
   );
