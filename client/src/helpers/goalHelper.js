c561c46bb3c2a00fbcbf911fd18ce5c45555ac0a
neethu-ms <msneethu2989@gmail.com>
2020-04-17 15:08:23 +0000

    Add timeline

--- client/src/helpers/goalHelper.js
+++ client/src/helpers/goalHelper.js
@@ -1,18 +1,29 @@
 // Finds previous user Goals ( All goals other than that of current day )
 import moment from 'moment';
 export function getPreviousUserGoals(userGoals, goals, currentDate, userId) {
-  const previousGoals = userGoals.filter(userGoal => {
+  let previousGoals = userGoals.filter(userGoal => {
     return (moment(userGoal.createdAt).format('MM-DD-YYYY') < moment(currentDate).format('MM-DD-YYYY')) && (userGoal.user_id == userId);
   }
   );
+  previousGoals = previousGoals.sort((goal1, goal2) => {
+    if (moment(goal1.createdAt) < moment(goal2.createdAt)) {
+      return 1;
+    } else {
+      return -1;
+    }
+  }
+  );
+  
   let expandedPreviousGoals = previousGoals.map((previousGoal) => {
     let expandedGoal = {};
-    expandedGoal.createdAt = previousGoal.createdAt;
+    expandedGoal.createdAt = moment(previousGoal.createdAt).format('LLLL');  // Formatted date 
     expandedGoal.question = goals[previousGoal.goal_id].question;
     expandedGoal.answer = previousGoal.answer;
     return expandedGoal;
 
   });
+
+  expandedPreviousGoals.sort((goal1, goal2) => goal1.createdAt > goal2.createdAt);
   return expandedPreviousGoals;
 
 };
