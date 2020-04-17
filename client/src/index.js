3c8ed34ed092e20938ed0d30263ade36931bb387
neethu-ms <msneethu2989@gmail.com>
2020-04-17 00:54:49 +0000

    Remove bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Application from './components/Application';
 import './index.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(
   <Application />,
