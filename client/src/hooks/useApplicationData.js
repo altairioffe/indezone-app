0d9c03b9d098abdcc782c331000dd723f63ad06f
neethu-ms <msneethu2989@gmail.com>
2020-04-17 23:34:19 +0000

    Add axios call for get

--- client/src/hooks/useApplicationData.js
+++ client/src/hooks/useApplicationData.js
@@ -3,15 +3,21 @@ import {useState, useEffect } from "react";
 export default function useApplicationData(){
    const [state,setState] = useState({
        userGoals:[],
-       goals:[]
+       goals:[],
+       biodatas:[],
+       users:[]
    });
 
  useEffect(() => {
      Promise.all([
            axios.get('/api/userGoals'),
-           axios.get('/api/goals')
+           axios.get('/api/goals'),
+           axios.get('/api/biodatas'),
+           axios.get('/api/users')
+
      ]).then((all) => {
-           setState((state) => ({...state,userGoals: all[0].data,goals:all[1].data}))
+           setState((state) => ({...state,userGoals: all[0].data,goals:all[1].data,biodatas:all[2].data,
+      users:all[3].data}))
      })
      .catch(err => err.message);
    },[]); 
