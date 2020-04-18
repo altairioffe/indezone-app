96f41b3975bc570f8a8594e3600332308a5b1cb7
Lucas <cekanlucas@gmail.com>
2020-04-17 20:10:55 -0400

    toggle logic and animation

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,15 +1,14 @@
 import "./QuestionAnswer/styles.scss";
 
 import React, { useState } from "react";
-import {Button, TextField} from '@material-ui/core';
+import { Button, TextField, Slide } from "@material-ui/core";
 
 export default function Navbar(props) {
-
   // Here are the states to keep track of login process
   const [loginState, setLoginState] = useState(0);
   const [loginEmail, setLoginEmail] = useState(null);
   const [loginPassword, setLoginPassword] = useState(null);
-  
+
   /* 
     State 0: Initial Login
       Login, Register, Email Field 
@@ -21,13 +20,18 @@ export default function Navbar(props) {
 
   const inputLoginEmail = (email) => {
     return email;
-  }
-  const login = (loginEmail) => [...props.users].find( (user) => {
-    console.log(loginEmail, user.email, user)
-    return user.email === inputLoginEmail(loginEmail).trim() ? 
-      true : false;
-  })
-
+  };
+  const login = (formInput) => {
+    if(loginState === 0){
+      [...props.users].find((user) => {
+        console.log(formInput, user.email, user);
+        return user.email === inputLoginEmail(formInput).trim() ? true : false;
+      })
+        ? setLoginState(1)
+        : console.log("error");
+      console.log(loginState);
+    }
+  };
 
   return (
     <header>
@@ -41,53 +45,60 @@ export default function Navbar(props) {
         <a>Find Others</a>
         <a>View Badges</a>
       </div>
-
-      <TextField 
-        id="outlined-basic"
-        name="emailInput"
-        label="Enter Email" 
-        variant="outlined"
-        color='primary'
-        autoFocus='true'
-        placeholder='example@email.com'
-        type='email'
-        onChange={(e) => setLoginEmail(e.target.value)}
-      />
-      <Button variant="fob">logout</Button>
-      
-      <Button 
-        onClick={"cancel"}
-        variant="outlined"
-        color="primary"
-        size="large"
-      >
-        Back
-      </Button>
-      <Button 
-        onClick={"cancel"}
-        variant="outlined"
-        color="primary"
-        size="large"
-      >
-        Register
-      </Button>
-      <Button 
-        onClick={() => console.log(login(loginEmail))}
-        variant="outlined"
-        color="primary"
-        size="large"
-      >
-        Login
-      </Button>
-      <Button 
-        onClick={() => console.log()}
-        variant="outlined"
-        color="primary"
-        size="large"
-      >
-        Logout
-      </Button>
-    
+      <Slide direction="left" in={loginState === 0} timeout={300}>
+        <TextField
+          id="outlined-basic"
+          name="emailInput"
+          label="Enter Email"
+          variant="outlined"
+          color="primary"
+          autoFocus="true"
+          placeholder="example@email.com"
+          type="email"
+          onChange={(e) => setLoginEmail(e.target.value)}
+        />
+      </Slide>
+      {/* <Button variant="fob">logout</Button> */}
+      <Slide direction="left" in={loginState === 0} timeout={300}>
+        <Button
+          onClick={"cancel"}
+          variant="outlined"
+          color="primary"
+          size="large"
+        >
+          Back
+        </Button>
+      </Slide>
+      <Slide direction="left" in={loginState === 0} timeout={300}>
+        <Button
+          onClick={"cancel"}
+          variant="outlined"
+          color="primary"
+          size="large"
+        >
+          Register
+        </Button>
+      </Slide>
+      <Slide direction="left" in={loginState === 0} timeout={300}>
+        <Button
+          onClick={() => console.log(login(loginEmail))}
+          variant="outlined"
+          color="primary"
+          size="large"
+        >
+          Login
+        </Button>
+      </Slide>
+      <Slide direction="left" in={loginState === 0} timeout={300}>
+        <Button
+          onClick={() => console.log()}
+          variant="outlined"
+          color="primary"
+          size="large"
+        >
+          Logout
+        </Button>
+      </Slide>
     </header>
   );
 }
