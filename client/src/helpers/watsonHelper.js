2e819e2d4e286d9f252abf190ece48440f3e3ecc
Altair <altair.ioffe@gmail.com>
2020-04-18 05:36:12 +0000

    comment out unused code

--- client/src/helpers/watsonHelper.js
+++ client/src/helpers/watsonHelper.js
@@ -1,16 +1,23 @@
 import {getPreviousUserGoals} from '../helpers/goalHelper';
 
-const currentUser = "6"; //temporary change 
+//const currentUser = "6"; //temporary change 
   
-const previousUserGoals = getPreviousUserGoals(state.userGoals,state.goals,new Date(),currentUser);
+//const previousUserGoals = getPreviousUserGoals(state.userGoals,state.goals,new Date(),currentUser);
+
+let sampleGoals = [{answer: "Be healthy"}, {answer: "Eat vegetables"}, {answer: "Read some books"}];
+
+//console.log(sampleGoals)
 
 
+const requestInsight = (userGoalsArray) => {
 Promise.resolve(
   axios
     .post("/api/userGoals", {
-      body: previousUserGoals
+      body: userGoalsArray
     })
     .then(response => {
       console.log("API CALL RESPONSE: ", response)
     })
-)
+  )
+}
+
