f76fafe89c5fa5f65660160a4ad30a44bd2f8464
neethu-ms <msneethu2989@gmail.com>
2020-04-19 21:51:44 +0000

    Add validation before computing current user goals

--- client/src/hooks/useApplicationData.js
+++ client/src/hooks/useApplicationData.js
@@ -1,62 +1,66 @@
-import axios from 'axios'; 
-import {useState, useEffect } from "react";
-import {getCurrentUserGoals} from '../helpers/goalHelper';
+import axios from 'axios';
+import { useState, useEffect } from "react";
+import { getCurrentUserGoals } from '../helpers/goalHelper';
 
-export default function useApplicationData(){
-   const [state,setState] = useState({
-       userGoals:[],
-       goals:[],
-       biodatas:[],
-       users:[],
-       currentUserGoals:[],
-       currentUser:6
-   });
+export default function useApplicationData() {
+  const [state, setState] = useState({
+    userGoals: [],
+    goals: [],
+    biodatas: [],
+    users: [],
+    currentUserGoals: [],
+    currentUser: null
+  });
 
- useEffect(() => {
-     Promise.all([
-           axios.get('/api/userGoals'),
-           axios.get('/api/goals'),
-           axios.get('/api/biodatas'),
-           axios.get('/api/users')
+  useEffect(() => {
+    Promise.all([
+      axios.get('/api/userGoals'),
+      axios.get('/api/goals'),
+      axios.get('/api/biodatas'),
+      axios.get('/api/users')
 
-     ]).then((all) => {
-           setState((state) => ({...state,userGoals: all[0].data,goals:all[1].data,biodatas:all[2].data,
-      users:all[3].data}))
-     })
-     .catch(err => err.message);
-   },[]); 
+    ]).then((all) => {
+      setState((state) => ({
+        ...state, userGoals: all[0].data, goals: all[1].data, biodatas: all[2].data,
+        users: all[3].data
+      }))
+    })
+      .catch(err => err.message);
+  }, []);
 
-  useEffect(() =>{
-    setState((state) => ({
-      ...state,
-      currentUserGoals: getCurrentUserGoals(state.userGoals, state.goals, state.currentUser)
-    }))
-  },[state.currentUser, state.userGoals]);
+  useEffect(() => {
+    if (state.currentUser != null && state.userGoals != null) {
+      setState((state) => ({
+        ...state,
+        currentUserGoals: getCurrentUserGoals(state.userGoals, state.goals, state.currentUser)
+      }))
+    }
+  }, [state.currentUser, state.userGoals]);
 
   const ansQuestion = (ans) => {
 
-    let answer =  {
-        user_id:8,
-        goal_id:1,
-        answer: ans
+    let answer = {
+      user_id: 8,
+      goal_id: 1,
+      answer: ans
     }
-    
+
     return axios
       .post(`/api/userGoals`, answer)
-      .then( () => {
+      .then(() => {
         console.log("useApplicationData")
         setState({
           ...state
         });
         return;
       })
-      .catch( () => {
+      .catch(() => {
         console.log('ERROR')
         return 'error'
       })
   }
 
-   return { 
+  return {
     ansQuestion,
     state
   };
