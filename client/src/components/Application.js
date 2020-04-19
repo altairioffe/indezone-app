110c57a0a48851f20a64548b99699f73f98c2c2e
neethu-ms <msneethu2989@gmail.com>
2020-04-19 21:34:11 +0000

    pass functions for add goal to children

--- client/src/components/Application.js
+++ client/src/components/Application.js
@@ -10,20 +10,13 @@ export default function Application() {
     
   const {
     ansQuestion,
-    state
+    state,
+    addUserGoal,
+    setAnswer
   } = useApplicationData();
     
-  const questionsArr = [
-    "Someone who needs me on my A-game today is...",
-    "A situation that might stress me out or trip me up today could be...",
-    "If I was a high performance coach looking at my life from a high level, I would tell myself to remember that ... ... and the way that my best self would deal with that is... ",
-    "The big progects I have to keep in mind that I want to take on, even if I can't act toward them today, are...",
-    "The big projects I have to keep in mind that I want to take on, even if I can't act toward them today, are...",
-    "I would know that today was a great success if at the end of the day I did, or felt this..."
-  ];
-  
-  const bio = "Everybody has the power to remodel their behaviour, habits, and attitudes, but not everybody knows how. Our app will make it simple and rewarding for anybody to get the benefits of reflective journaling. Our app will bring people together through personal goals, challenges, and insights, so that we can realize our potential together."
-
+  const questionsArr = state.goals.map((goal) => goal.question);
+  const bio = state.biodatas.filter((biodata) => biodata.user_id === state.currentUser);
   return (
     <main className="layout">
       <Navbar 
@@ -38,6 +31,9 @@ export default function Application() {
         <QuestionList 
           giveAnswer={ansQuestion}
           questions={questionsArr}
+          setAnswer = {setAnswer}
+          addUserGoal = {addUserGoal}
+          goals = {state.goals}
         />
         <hr />
         <div>
