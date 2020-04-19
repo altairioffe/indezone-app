c2ed7539d594dd1fb4211aaa9108d9bb414c91ea
Altair <altair.ioffe@gmail.com>
2020-04-19 19:37:07 +0000

    refactor to render conditionally only if user is logged in

--- client/src/components/Wall.js
+++ client/src/components/Wall.js
@@ -5,12 +5,13 @@ import  {styles}  from './WallStyles';
 import '../styles/Wall.scss';
 export default function Wall(props) {
   const userGoals = props.userGoals;
-  const userGoalsView = userGoals.map(userGoal => {
+  if (userGoals) { const userGoalsView = userGoals.map(userGoal => {
     return (
       <WallItem key={userGoal.id} createdAt={userGoal.createdAt} question={userGoal.question} answer={userGoal.answer} />
     );
   }
   );
+
 
   return (
     <div class ="timeline">
@@ -19,4 +20,9 @@ export default function Wall(props) {
       </Timeline>
     </div>
   );
+}
+else 
+return (
+<WallItem question={"you need to log in"} />
+)
 }
