45563f0cc8aecb06747b8eadadd7183f4038843c
Altair <altair.ioffe@gmail.com>
2020-04-20 20:32:26 +0000

    switch datatypes from string to text in models

--- backend/db/models/biodata.js
+++ backend/db/models/biodata.js
@@ -2,7 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   const Biodata = sequelize.define('biodata', {
     name: DataTypes.STRING,
-    text: DataTypes.STRING,
+    text: DataTypes.TEXT,
     user_id: DataTypes.INTEGER
   }, {});
   Biodata.associate = function(models) {
--- backend/db/models/goal.js
+++ backend/db/models/goal.js
@@ -1,8 +1,8 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Goal = sequelize.define('goal', {
-    question: DataTypes.STRING,
-    suggestion: DataTypes.STRING
+    question: DataTypes.TEXT,
+    suggestion: DataTypes.TEXT
   }, {});
   Goal.associate = function(models) {
     // associations can be defined here
