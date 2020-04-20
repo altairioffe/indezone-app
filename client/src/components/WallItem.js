4113d9c650ca1f58b4ae37c4e1af5ee8e39c141b
neethu-ms <msneethu2989@gmail.com>
2020-04-20 19:49:52 +0000

    Add collapsible option

--- client/src/components/WallItem.js
+++ client/src/components/WallItem.js
@@ -6,7 +6,7 @@ export default function WallItem(props) {
  
   return (
 
-    <TimelineEvent contentStyle={styles.contentStyle} container="card" bubbleStyle={styles.bubbleStyle} key={props.createdAt} title={props.question?props.question:""}
+    <TimelineEvent collapsible= { true } contentStyle={styles.contentStyle} container="card" bubbleStyle={styles.bubbleStyle} key={props.createdAt} title={props.question?props.question:""}
       createdAt={props.createdAt}
       icon={<i className="material-icons md-18"></i>}
     >
