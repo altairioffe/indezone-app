b1808aad4bc152267f69f88cc8c2bdcbd75a1db5
neethu-ms <msneethu2989@gmail.com>
2020-04-21 17:11:12 +0000

    Update seed data

--- backend/db/seeders/20200412211918-demo-user-goal.js
+++ backend/db/seeders/20200412211918-demo-user-goal.js
@@ -8,33 +8,33 @@ module.exports = {
 
     return queryInterface.bulkInsert('user_goals', [{
       id:1,
-      answer:"Be healthy",
       user_id:6,
       goal_id:1,
+      answer:"Be healthy",
       createdAt: new Date().toISOString(),
       updatedAt: new Date().toISOString()
     },
     {
       id:2,
-      answer:"Eat vegetables",
       user_id:6,
       goal_id:2,
+      answer:"Eat vegetables",
       createdAt: new Date().toISOString(),
       updatedAt: new Date().toISOString()
     },
     {
       id:3,
-      answer:"Be healthy",
       user_id:7,
       goal_id:1,
+      answer:"Be healthy",
       createdAt: new Date().toISOString(),
       updatedAt: new Date().toISOString()
     },
     {
       id:4,
-      answer:"Eat vegetables",
       user_id:7,
       goal_id:2,
+      answer:"Eat vegetables",
       createdAt: new Date().toISOString(),
       updatedAt: new Date().toISOString()
     },
