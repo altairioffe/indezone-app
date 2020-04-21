bf7687343048e27ce55a8307fdf2cb36aa49b128
Altair <altair.ioffe@gmail.com>
2020-04-21 19:55:40 +0000

    replace questions and suggestions

--- backend/db/seeders/20200412210112-demo-goal.js
+++ backend/db/seeders/20200412210112-demo-goal.js
@@ -8,111 +8,110 @@ module.exports = {
 
     return queryInterface.bulkInsert('goals', [{
       id:1,
-      question: 'What is your goal today?',
-      suggestion: 'Do some productive task',
+      question: 'What is your main goal for today?',
+      suggestion: 'Describe why this goal matters to you',
       createdAt: new Date().toISOString(),
       updatedAt: new Date().toISOString()
     },
     {
       id:2,
-      question: 'What do you want to do to be healthy?',
-      suggestion: 'Do some exercise today',
+      question: 'If one word could describe the person I want to be today...',
+      suggestion: 'Pick a word, and go in detail about why that matters to you',
       createdAt: new Date().toISOString(),
       updatedAt: new Date().toISOString()
     },
     {
       id:3,
-      question: 'What do you want to do to improve your work?',
-      suggestion: 'Learn something new',
+      question: 'Who can I reach out to today for mentorship? With what?',
+      suggestion: 'Stand on the shoulders of giants (or just friends/colleagues). How can you leverage them to achieve more?',
       createdAt: new Date().toISOString(),
       updatedAt: new Date().toISOString()
     },
     {
       id:4,
-      question: 'What did you want to do be happy?',
-      suggestion: 'Help yourself or help someone',
+      question: 'One thing that I can get excited about for today is...',
+      suggestion: 'What are you looking forward to, and why is that important to you?',
       createdAt: new Date().toISOString(),
       updatedAt: new Date().toISOString()
     },
     {
       id:5,
       question: 'Someone who needs me on my A-game today is...',
-      suggestion: 'Think of a loved one',
+      suggestion: 'Who might be counting on you to be at your best? What can you do to support them?',
       createdAt: new Date().toISOString(),
       updatedAt: new Date().toISOString()
     },
     {
       id:6,
       question: 'A situation that might stress me out or trip me up today could be...',
-      suggestion: 'What tripped you up in the past?',
+      suggestion: 'What would be the best way to handle it?',
       createdAt: new Date().toISOString(),
       updatedAt: new Date().toISOString()
     },
     {
       id:7,
-      question: 'If I was a high performance coach looking at my life from a high level, I would tell myself to remember that ... ... and the way that my best self would deal with that is...',
-      suggestion: 'Put yourself in a personal coach\'s shoes',
+      question: 'What can I do to make today more productive?',
+      suggestion: 'What are some things that might be holding you back?',
       createdAt: new Date().toISOString(),
       updatedAt: new Date().toISOString()
     },
     {
       id:8,
-      question: 'The big progects I have to keep in mind that I want to take on, even if I can\'t act toward them today, are...',
-      suggestion: 'Think BIG, you can do it!',
+      question: 'The big projects that I want to take on, even if I can\'t act toward them today, are...',
+      suggestion: 'Think BIG! What might help you along the way?',
       createdAt: new Date().toISOString(),
       updatedAt: new Date().toISOString()
     },
     {
       id:9,
-      question: 'I would know that today was a great success if at the end of the day I did, or felt this...',
-      suggestion: 'What did previous successful days look like?',
+      question: 'What does a great day look like to you?',
+      suggestion: 'Spending time outside, a spontaneous adventure... describe what it means to you',
       createdAt: new Date().toISOString(),
       updatedAt: new Date().toISOString()
     },
     {
       id:10,
-      question: 'A moment that I really appreciated today was...',
-      suggestion: 'Try listing your accomplishments for today',
+      question: 'What can I can do differently to feel healthier today?',
+      suggestion: 'Try yoga, avoid sugar, take a nap, get a massage...',
       createdAt: new Date().toISOString(),
       updatedAt: new Date().toISOString()
     },
     {
       id:11,
-      question: 'A situation or task I handled well today was...',
-      suggestion: 'What problems did you face... ...and how did you overcome them?',
+      question: 'What new habit would make me feel better every day?',
+      suggestion: 'Going to bed earlier, setting time aside to read a book...',
       createdAt: new Date().toISOString(),
       updatedAt: new Date().toISOString()
     },
     {
       id:12,
-      question: 'Something I realized or learned today was...',
-      suggestion: 'Keep track of lessons learned',
+      question: 'Looking back on yesterday, what can I do differently to get the most out of today?',
+      suggestion: 'What are some lessons you can apply?',
       createdAt: new Date().toISOString(),
       updatedAt: new Date().toISOString()
     },
     {
       id:13,
-      question: 'I could have made today even better if I...',
-      suggestion: 'Think of the perfect day',
+      question: 'How would you want someone to describe you after first meeting you?',
+      suggestion: 'What kind of impression to do you want to make? How can you make it?',
       createdAt: new Date().toISOString(),
       updatedAt: new Date().toISOString()
     },
     {
       id:14,
-      question: 'Something that could have helped me feel more connected to others today would have been...',
-      suggestion: 'Who do you want to connect to more?',
+      question: 'What are some positive changes you can make today?',
+      suggestion: 'List some things that within your control. What would be different if you make those changes?',
       createdAt: new Date().toISOString(),
       updatedAt: new Date().toISOString()
     },
     {
       id:15,
-      question: 'If I was my own high performance coach, I would tell myself this statement about today...',
-      suggestion: 'Put yourself in a personal coach\'s shoes',
+      question: 'If you had one extra hour today, what would you spend it on?',
+      suggestion: 'Is everything else in your day more important than doing this?',
       createdAt: new Date().toISOString(),
       updatedAt: new Date().toISOString()
     }
     
-  
     ]);
   },
   down: (queryInterface, Sequelize) => {
