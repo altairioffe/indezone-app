c24290fff5726ac0b26b05d785a02193f06b5505
Lucas <cekanlucas@gmail.com>
2020-04-22 09:54:13 -0400

    added card component when users have no more questions available on question feed

--- client/src/components/QuestionList.js
+++ client/src/components/QuestionList.js
@@ -2,6 +2,7 @@ import "./QuestionAnswer/styles.scss";
 
 import React, { useState } from "react"
 import { makeStyles } from '@material-ui/core/styles';
+import { Card, CardActions, CardContent, Typography, Button } from "@material-ui/core";
 import AnswerQuestionPanel from "./QuestionAnswer/AnswerQuestionPanel"
 
 import Slide from '@material-ui/core/Slide';
@@ -48,7 +49,25 @@ export default function QuestionFeed(props) {
     <section>
       <h1 className={classes.root}>Daily Question Feed <small>Write Entries Here</small></h1>
       <br/>
-      {questionsFilteredList}
+      {!questionsFilteredList.length === 0 ? questionsFilteredList : (
+    <Card className={classes.root} variant="contained-primary">
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Congratulations
+        </Typography>
+        <Typography variant="h5" component="h2">
+          You have Answered all your Questions for the Day 
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          Come Back Tommorrow for More
+        </Typography>
+        <Typography variant="body2" component="p">
+          Raise your Level to get more Questions
+        </Typography>
+      </CardContent>
+    </Card>
+  )
+}
       <br/>
     </section>
   )
