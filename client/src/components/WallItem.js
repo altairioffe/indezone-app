824847658cfa055ce6024584a58d88e2db45587f
neethu-ms <msneethu2989@gmail.com>
2020-04-23 12:05:38 +0000

    Modify props and style

--- client/src/components/WallItem.js
+++ client/src/components/WallItem.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { TimelineEvent } from 'react-event-timeline';
 import  {styles}  from './WallStyles';
+import { Button } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 export default function WallItem(props) {
  
   return (
 
-    <TimelineEvent contentStyle={styles.contentStyle} container="card" bubbleStyle={styles.bubbleStyle} key={props.createdAt} title={props.question?props.question:""}
+    <TimelineEvent buttons={<Button onClick={() => props.handleDelete(props.id)}><DeleteIcon/></Button>} cardHeaderStyle = {styles.cardHeaderStyle} contentStyle={styles.contentStyle} container="card" bubbleStyle={styles.bubbleStyle} key={props.createdAt} title={props.question?props.question:""}
       createdAt={props.createdAt}
       icon={<i className="material-icons md-18"></i>}
     >
