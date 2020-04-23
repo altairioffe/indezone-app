d9a2c8180a93634a9569d1e30fc874072f81a3cb
neethu-ms <msneethu2989@gmail.com>
2020-04-23 12:07:36 +0000

    Modify style

--- client/src/components/Bio/Profile.js
+++ client/src/components/Bio/Profile.js
@@ -7,15 +7,17 @@
 
 import React from "react";
 import "./Profile.scss"
+import {Container} from '@material-ui/core';
 
 export default function UserBio(props){
 
 
   return (
     <aside className="" id="">
-      <div id="">
-        <img id="" src={"https://image.freepik.com/free-photo/woman-smiling-face-with-curly-hair_1139-582.jpg"} alt="profile pic"/>
-      </div>
+      <Container>
+       {/* <img id="" src={"https://image.freepik.com/free-photo/woman-smiling-face-with-curly-hair_1139-582.jpg"} alt="profile pic"/> */}
+  {/* <img src="images/journaling1.jpg" alt="home-logo"></img> */}
+        </Container>
       <hr className="seperator"/>
       <section className="prompt__actions card_right">
           <h4 className="">Level: {props.level}</h4>
