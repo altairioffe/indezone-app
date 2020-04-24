37d193b29d54c260863ad8e134b69bd3f0878cb5
neethu-ms <msneethu2989@gmail.com>
2020-04-24 23:54:21 +0000

    Modify button

--- client/src/components/Bio/UserBio.js
+++ client/src/components/Bio/UserBio.js
@@ -1,16 +1,18 @@
 import React from 'react';
-
+import '../../styles/Bio.scss';
+import {Button} from '@material-ui/core';
 export default function UserBio(props) {
 
   return(
-    <article className="">
+    <article className="user-bio">
     <p>{props.bio}</p>
-    <button 
-       className=""
+    <Button 
+       variant="contained" 
+       color="secondary"
        onClick={props.onClick}
        disabled={props.disabled}>
          GET INSIGHTS
-     </button>
+     </Button>
   </article>
   )
 }
