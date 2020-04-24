445e7165e04a6b69fbc5c1e327c57a74dd57fde2
neethu-ms <msneethu2989@gmail.com>
2020-04-24 23:46:08 +0000

    Add style

--- client/src/components/Bio/Insights.js
+++ client/src/components/Bio/Insights.js
@@ -1,17 +1,19 @@
 import React from 'react';
-
+import {Button} from '@material-ui/core';
 export default function Insights(props) {
 
   return(
     <article>
-      <h4> Here is an analysis of your entries: </h4>
+      <h4 className="insight-heading"> Here is an analysis of your entries: </h4>
       <p>{props.insights}</p>
-      <button 
-       className=""
+      <Button 
+      variant="contained" 
+      color="secondary"
+       className="button"
        onClick={props.onCancel}
        disabled={props.disabled}>
          BACK
-     </button>
+     </Button>
     </article>
   )
 }
