472d384043c0761f7857c114ed8065bb5079f902
Lucas <cekanlucas@gmail.com>
2020-05-02 00:08:06 -0400

    rolled back changes in form component

--- client/src/components/QuestionAnswer/Form.js
+++ client/src/components/QuestionAnswer/Form.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Collapse from '@material-ui/core/Collapse';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 
